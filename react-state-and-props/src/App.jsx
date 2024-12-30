0db540448ac35b482d85132cf5972d69c3fdce98
joshuaharris07 <joshua.harris07@gmail.com>
2024-12-30 16:55:39 -0700

    Added fixed code for task 1 and beginning of task 2

---
+++ react-state-and-props/src/App.jsx
@@ -0,0 +1,14 @@
+import Task1 from './components/Task1'
+import MoviesList from './components/MoviesList'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Task1 />
+      <MoviesList />
+    </div>
+  )
+}
+
+export default App
