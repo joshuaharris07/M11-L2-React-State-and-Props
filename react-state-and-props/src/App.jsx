3c61db6c484d730285b710668be886a2f2d0127a
joshuaharris07 <joshua.harris07@gmail.com>
2024-12-30 21:48:42 -0700

    Cleaned up commented out parts.

--- react-state-and-props/src/App.jsx
+++ react-state-and-props/src/App.jsx
@@ -4,16 +4,10 @@ import MoviesList from './components/MoviesList'
 import './App.css'
 
 const App = () => {
-  // const [showMovies, setShowMovies] = useState(true)
-  // const movies = useState(['The Lord of the Rings', 'Star Wars', 'Moana'])
-
-
-
   return (
     <div>
       <Task1 />
       <MoviesList />
-      {/* {showMovies && <MoviesList items={movies} />} */}
     </div>
   );
 }
